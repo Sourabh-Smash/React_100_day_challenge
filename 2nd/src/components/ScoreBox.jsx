dd8a03a8e73a08b5e991aaacd2efa66feb9eedbb
Sourabh Manawat <sourabhmanawat@gmail.com>
2023-10-22 22:02:03 +0530

    Updating 2nd project scoreBox component

--- 2nd/src/components/ScoreBox.jsx
+++ 2nd/src/components/ScoreBox.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ScoreBox = ({ totalTime }) => {
   const changeTime = () => {
     return totalTime === "easy" ? 5 : totalTime === "medium" ? 3 : 2;
   };
-  const [changet,setChangeT]=useState(changeTime)
-  console.log(totalTime);
+  const [changet, setChangeT] = useState(changeTime);
+  useEffect(() => {
+    const calcInterval = setInterval(() => {
+      setChangeT((prevnum) => {
+        if (prevnum > 0) {
+          return prevnum - 1;
+        } else {
+          clearInterval(calcInterval);
+          return 0;
+        }
+      });
+    }, 1000);
+    return () => clearInterval(calcInterval);
+  }, changeTime);
+  // console.log(totalTime);
   return (
     <div className="scores">
       <h3>
