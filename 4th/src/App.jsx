8f789b7bafb1c0756e1f9dc39b642e4990e07032
Sourabh Manawat <souramena123@gmail.com>
2023-12-31 21:23:02 +0530

    setup 4th password generator project

---
+++ 4th/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center mt-10 text-white'>Password Generator </h1>
+    </>
+  )
+}
+
+export default App
