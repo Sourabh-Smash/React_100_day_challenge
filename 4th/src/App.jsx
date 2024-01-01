d1b0d992c207cf51b3fca266fa676a9eff5be615
Sourabh Manawat <souramena123@gmail.com>
2024-01-01 18:16:10 +0530

    structore of password field

--- 4th/src/App.jsx
+++ 4th/src/App.jsx
@@ -1,13 +1,22 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-4xl font-bold text-center mt-10 text-white'>Password Generator </h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 ">
+        <h1 className="text-white text-center">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
+          <input
+            type="text"
+            // value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
