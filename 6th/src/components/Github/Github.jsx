36b399115a8517922bb422dbd3ee8a33346818fc
Sourabh Manawat <souramena123@gmail.com>
2024-01-04 18:57:49 +0530

    completed react router project

---
+++ 6th/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Github = () => {
+  return (
+    <div className="bg-gray-600 text-white text-center m-4 p-4 text-3xl ">
+      Github
+    </div>
+  );
+};
+
+export default Github;
